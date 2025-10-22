042d10e970f09d6fb9a904b9d11ea636aed4eaee
Bartosz Sośnica <115103008+Zvki@users.noreply.github.com>
2025-10-22 02:11:09 +0200

    feat: add custom button component

---
+++ client/src/app/shared/ui/button/consts/button-variant.type.ts
@@ -0,0 +1,11 @@
+export type ButtonVariantType = 'default' | 'outline' | 'link' | 'ghost' | 'destructive'
+
+export const ButtonBase: string = "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 focus-visible:ring-offset-slate-900 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2"
+
+export const ButtonVariants: Record<ButtonVariantType, string> = {
+  default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+  outline: 'border border-input bg-transparent hover:bg-accent hover:text-accent-foreground',
+  link: 'text-foreground underline-offset-4 hover:underline',
+  ghost: 'hover:bg-accent hover:text-accent-foreground',
+  destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90'
+}
