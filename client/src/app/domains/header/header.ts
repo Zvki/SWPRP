4dd6f80c5ff11d84382edddbcdd1184e0c40025f
Bartosz Sośnica <115103008+Zvki@users.noreply.github.com>
2025-10-22 02:12:49 +0200

    refactor: change buttons and styling

--- client/src/app/domains/header/header.ts
+++ client/src/app/domains/header/header.ts
@@ -1,23 +1,20 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {Button} from '../../shared/ui/button/button';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    Button
   ],
   styleUrl: './header.css'
 })
 export class Header {
 
   user: any = { name: 'Tester' };
-  isMobileMenuOpen = false;
-
-  toggleMobileMenu() {
-    this.isMobileMenuOpen = !this.isMobileMenuOpen;
-  }
 
   onLogout() {
     console.log('Wylogowano');
