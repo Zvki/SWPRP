ac15aaf537f378686dabcead5a564ca32b39ad88
Bartosz Sosnica <bartoszsosnica@gmail.com>
2025-11-09 21:04:47 +0100

    feat: Add landing page with main section, styles, and routing changes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import {Dashboard} from './domains/dashboard/dashboard';
+import {Main} from './domains/main/main';
 
 export const routes: Routes = [
-  {path: "", component: Dashboard}];
+  {path: "", component: Main},
+  {path: "dashboard", component: Dashboard}];
