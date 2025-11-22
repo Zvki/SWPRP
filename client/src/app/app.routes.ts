d287e3f05c5629ebe7c997e2a277adc14836b1b9
Bartosz Sosnica <bartoszsosnica@gmail.com>
2025-11-22 20:59:34 +0100

    feat: Add route for project details with dynamic ID handling

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import {Dashboard} from './domains/dashboard/dashboard';
 import {Main} from './domains/main/main';
+import {ProjectDetails} from './domains/project-details/project-details';
 
 export const routes: Routes = [
   {path: "", component: Main},
-  {path: "dashboard", component: Dashboard}];
+  {path: "dashboard", component: Dashboard},
+  {path: "project/:id", component: ProjectDetails}
+];
