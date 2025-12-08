e2ad9d150b1e210e2e4e8a0ac4eff9d3d0c61e44
Bartosz Sosnica <bartoszsosnica@gmail.com>
2025-12-08 22:00:17 +0100

    feat: Add supervisor role-based file management and improve `Main` layout with dynamic headers and conditional messages

--- client/src/app/domains/main/main.ts
+++ client/src/app/domains/main/main.ts
@@ -1,6 +1,9 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, effect, inject} from '@angular/core';
 import {ActivityService} from '../../core/services/api/activity.service';
 import {ActivityCard} from '../../shared/activity-card/activity-card';
+import {AuthService} from '../../core/services/api/auth.service';
+import {UserRole} from '../../core/interfaces/user-response';
+import {FileStatus} from '../../core/interfaces/activity/activity.interface';
 
 @Component({
   selector: 'app-main',
@@ -10,16 +13,27 @@ import {ActivityCard} from '../../shared/activity-card/activity-card';
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
-export class Main implements OnInit{
+export class Main{
 
   private readonly acitvityService = inject(ActivityService);
+  protected readonly user = inject(AuthService).user;
   protected readonly pendingFiles = this.acitvityService.pendingFiles;
   protected readonly nextMeetings = this.acitvityService.nextMeetings;
+  protected isSupervisor = this.user()?.role === UserRole.SUPERVISOR;
 
-  public ngOnInit(): void {
-    this.acitvityService.loadPendingFiles()
-    this.acitvityService.loadNextMeetings()
+  constructor() {
+    effect(() => {
+      const user = this.user();
+      if (!user) return;
+
+      const status = user.role === UserRole.SUPERVISOR
+        ? FileStatus.PENDING
+        : FileStatus.CHANGES_REQUESTED;
+
+      this.acitvityService.loadFilesByStatus(status);
+      this.acitvityService.loadNextMeetings();
+    });
   }
 
-
+  protected readonly UserRole = UserRole;
 }
